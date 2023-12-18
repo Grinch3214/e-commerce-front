2b5ab014fdf2670f51ff312d19a8110708ff8f69
Max <scorpiomax90@gmail.com>
2023-12-18 14:42:10 +0200

    adding logic to a component product card and product view

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,3 +1,14 @@
 <template>
-	<div>Catalog (btn View details) (btn Add to Cart)</div>
+	<div>
+		<ProductCardVue v-for="item in list" :key="item.id" :product="item" />
+	</div>
 </template>
+
+<script setup>
+	import { computed } from 'vue'
+	import ProductCardVue from '../components/ProductCard.vue'
+	import { useProductsStore } from '../store/index'
+
+	const products = useProductsStore()
+	const list = computed(() => products.listProduct)
+</script>
