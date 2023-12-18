e798c94cf22ca2f0e66473ed69bcc087fea8ad62
Max <scorpiomax90@gmail.com>
2023-12-18 14:08:07 +0200

    Setting up and initializing the store, setting up an API request

--- src/store/index.js
+++ src/store/index.js
@@ -1,2 +1,22 @@
-// import { ref } from 'vue'
-// import { defineStore } from 'pinia'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import { BASE_API } from '../constans'
+import { ref, computed } from 'vue'
+
+export const useProductsStore = defineStore('productsStore', () => {
+
+	const products = ref([])
+	const listProduct = computed(() => products.value)
+
+	const fetchProducts = async() => {
+		await axios.get(`${BASE_API}`)
+			.then((response) => {
+				products.value = response.data
+			}).catch(err => console.log(err))
+	}
+
+	return {
+		fetchProducts,
+		listProduct
+	}
+})
