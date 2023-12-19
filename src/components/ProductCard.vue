b7d563b072b1a62203eb1a0f16ac23921a1959b3
Max <scorpiomax90@gmail.com>
2023-12-19 18:21:29 +0200

    added styles for buttons

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -6,9 +6,9 @@
 			<h3>{{ product.title }}</h3>
 			<p>{{ product.category }}</p>
 			<p>{{ product.price }}</p>
-			<div>
-				<button>View product</button>
-				<button>Add to Cart</button>
+			<div class="flex gap-5">
+				<button class="px-7 py-4 bg-gray-900 text-white hover:bg-gray-800 transition-colors">View product</button>
+				<button class="px-7 py-4 text-gray-900 border-2 transition-colors border-gray-900 hover:bg-slate-100">Add to Cart</button>
 			</div>
 		</article>
 </template>
