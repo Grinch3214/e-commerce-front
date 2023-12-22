e48a2f17592859b8d7c7a47127e7ce6a34473d7d
Max <scorpiomax90@gmail.com>
2023-12-22 17:33:39 +0200

    create product item component

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import ProductsView from '../views/ProductsView.vue'
+import ProductItem from '../components/ProductItem.vue'
 
 const routes = [
 	{
 		path: '/',
 		name: 'ProductsView',
 		component: ProductsView
+	},
+	{
+		path: '/product/',
+		name: 'ProductItem',
+		component: ProductItem
 	}
 ]
 
