58adc66c866d2a04b5413234552a4f1b87c5ee69
Max <scorpiomax90@gmail.com>
2023-12-28 15:24:24 +0200

    added adaptive for cards

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<article class="flex flex-col border border-gray-800 rounded-lg p-6 gap-2 group">
+	<article class="w-full max-w-sm md:max-w-none flex flex-col border border-gray-800 rounded-lg p-6 gap-2 group">
 			<div class="h-40 mx-auto mb-3 overflow-hidden">
 				<img class="group-hover:scale-105 w-auto h-full object-cover transition-all" :src="product.image" alt="" width="200" height="200">
 			</div>
@@ -9,9 +9,9 @@
 				<p class="text-sm">Price:</p>
 				<p class="">${{ product.price }}</p>
 			</div>
-			<div class="flex gap-4">
-				<button class="rounded-lg px-4 py-2 bg-gray-900 text-white hover:bg-gray-800 transition-colors w-3/6">View product</button>
-				<button class="rounded-lg px-4 py-2 text-gray-900 border-2 transition-colors border-gray-900 hover:bg-slate-100 w-3/6">Add to Cart</button>
+			<div class="flex flex-col lg:flex-row gap-4">
+				<button class="rounded-lg px-4 py-2 bg-gray-900 text-white hover:bg-gray-800 transition-colors w-full lg:w-3/6">View product</button>
+				<button class="rounded-lg px-4 py-2 text-gray-900 border-2 transition-colors border-gray-900 hover:bg-slate-100 w-full lg:w-3/6">Add to Cart</button>
 			</div>
 		</article>
 </template>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,7 +2,7 @@
 	<div v-if="!products.loadProducts">
 		Download product information
 	</div>
-	<div v-else class="grid grid-cols-4 container gap-4 py-10">
+	<div v-else class="grid grid-cols-1 justify-items-center md:justify-items-stretch md:grid-cols-2 xl:grid-cols-3 xxl:grid-cols-4 container gap-4 py-10">
 		<ProductCard v-for="item in list" :key="item.id" :product="item" />
 	</div>
 </template>
