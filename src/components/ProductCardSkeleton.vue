69bbd4e56cd83f1ae4a9e6ded3707f0b7245d290
Max <scorpiomax90@gmail.com>
2024-01-02 22:57:33 +0200

    added animated placeholder component

--- src/components/ProductCardSkeleton.vue
+++ src/components/ProductCardSkeleton.vue
@@ -1,5 +1,15 @@
 <template>
-	<div>
-		Download product information
-	</div>
+	<div class="w-full max-w-sm md:max-w-none flex flex-col border border-gray-800 rounded-lg p-6 gap-2 group">
+			<div class="h-40 mx-auto mb-3 overflow-hidden"></div>
+			<div class="text-xl font-bold mb-auto"></div>
+			<p class="text-xs py-1 px-4 bg-sky-950 self-start text-white rounded-lg"></p>
+			<div class="flex justify-between items-center font-bold">
+				<p class="text-sm"></p>
+				<p class=""></p>
+			</div>
+			<div class="flex flex-col lg:flex-row gap-4 text-center">
+				<div class="rounded-lg px-4 py-2 bg-gray-900 text-white hover:bg-gray-800 transition-colors w-full lg:w-3/6 border-2 border-gray-900"></div>
+				<div class="rounded-lg px-4 py-2 text-gray-900 border-2 transition-colors border-gray-900 hover:bg-slate-100 w-full lg:w-3/6"></div>
+			</div>
+		</div>
 </template>
