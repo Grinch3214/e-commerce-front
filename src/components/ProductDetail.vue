7cff69112a246e870259c8b6013785ebf1f62536
Max <scorpiomax90@gmail.com>
2024-01-04 09:27:58 +0200

    added adaptive for product detail

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -1,7 +1,7 @@
 <template>
 	<section class="container px-4 py-10">
-		<div class="flex gap-5">
-			<div class="h-[300px] max-w-[400px] w-full flex-shrink-0 flex justify-center">
+		<div class="lg:flex gap-5">
+			<div class="h-[300px] max-w-[400px] w-full flex-shrink-0 flex justify-center mx-auto mb-5 lg:m-0">
 				<img
 					:src="product.image"
 					:alt="product.title"
@@ -13,18 +13,21 @@
 				>
 			</div>
 			<div>
-				<h2 class="font-bold text-3xl mb-5">{{ product.title }}</h2>
-				<p class="mb-3">{{ product.description }}</p>
-				<div class="flex gap-7 items-center mb-8">
+				<h2 class="font-bold text-3xl mb-5 text-center lg:text-left">{{ product.title }}</h2>
+				<p class="mb-3 text-justify">{{ product.description }}</p>
+				<div class="flex gap-7 items-center mb-2">
 					<p>{{ product.rating ? product.rating.rate : 'N/A' }}</p>
-					<p class="text-sm py-1 px-4 bg-sky-950 self-start text-white rounded-lg">{{ product.category }}</p>
+					<p class="text-xs py-1 px-4 bg-sky-950 self-start text-white rounded-lg">{{ product.category }}</p>
 				</div>
-				<div class="flex items-center gap-2 mb-10">
-					<button class="bg-gray-900 text-white w-10 h-7 rounded-lg">-</button>
-					<div class="border min-w-[28px] flex justify-center items-center rounded-lg px-2">0</div>
-					<button class="bg-gray-900 text-white w-10 h-7 rounded-lg">+</button>
+				<p class="font-bold mb-10">${{ product.price }}</p>
+				<div class="sm:flex justify-between items-center">
+					<div class="flex items-center gap-2 mb-5 sm:mb-0">
+						<button class="bg-gray-900 text-white w-10 h-7 rounded-lg">-</button>
+						<div class="border min-w-[28px] flex justify-center items-center rounded-lg px-2">0</div>
+						<button class="bg-gray-900 text-white w-10 h-7 rounded-lg">+</button>
+					</div>
+					<button class="rounded-lg px-4 py-2 bg-gray-900 text-white hover:bg-gray-800 transition-colors border-2 border-gray-900 w-full sm:w-auto">Add to cart</button>
 				</div>
-				<button class="rounded-lg px-4 py-2 bg-gray-900 text-white hover:bg-gray-800 transition-colors border-2 border-gray-900">Add to cart</button>
 			</div>
 		</div>
 	</section>
@@ -41,8 +44,5 @@
 	onMounted(() => products.fetchProductId(route.params.id))
 
 	const product = computed(() => products.productItem)
-	// setTimeout(() => {
-	// 	console.log(product.value)
-	// }, 1000)
 
 </script>
